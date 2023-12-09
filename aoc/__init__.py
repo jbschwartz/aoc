1e74b0b27aa8dbbf598561bbb03ddd44efa73a47
James Schwartz <jbschwartz@users.noreply.github.com>
2023-12-09 09:47:13 -0500

    Set logging to `DEBUG` by default.

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -5,6 +5,6 @@ from rich.logging import RichHandler
 
 logging.basicConfig(
     format="%(message)s",
-    level=logging.INFO,
+    level=logging.DEBUG,
     handlers=[RichHandler(console=Console(stderr=True, width=120))],
 )
